b5929ac18aaac0b4a15ee5cfc7d0552840e45195
Ale579 <alessandrotg9@gmail.com>
2024-05-15 16:49:38 +0200

    Make calculator UI response

--- logic.js
+++ logic.js
@@ -1,18 +1,56 @@
-let a = 3;
-let operator = "+";
-let b = 6;
+let a;
+let operator;
+let b;
+let result;
 
 let displayValue = document.querySelector("#display");
+displayValue.textContent = "0";
 
+// Calculator logic and track values
+// Change number display
 let numbers = document.querySelectorAll(".number");
 
-// Change number display
-numbers.forEach(number => {
-    number.addEventListener("click", event => {
+numbers.forEach(button => {
+    button.addEventListener("click", event => {
+        if (displayValue.textContent == "0") displayValue.textContent = "";
+        if (operator) displayValue.textContent = "";
         displayValue.textContent = displayValue.textContent + event.target.textContent;
     });
 })
 
+let operators = document.querySelectorAll(".operator");
+
+operators.forEach(button => {
+    button.addEventListener("click", event => {
+
+        operator = event.target.textContent;
+
+        if (!a) {
+            a = parseFloat(displayValue.textContent);
+        }
+
+        console.log(`a: ${a}`);
+        console.log(operator);
+    });
+})
+
+let equal = document.querySelector("#equal");
+
+equal.addEventListener("click", event => {
+    if (!b) {
+        b = parseFloat(displayValue.textContent);
+        result = operate(operator, a, b);
+        displayValue.textContent = result;
+
+        console.log(`b: ${b}`)
+        console.log(`result: ${result}`);
+
+        operator = null;
+        a = null;
+        b = null;
+
+    }
+})
 
 
 function add (a, b) {
@@ -43,5 +81,3 @@ function operate (operator, a, b) {
             return divide(a, b);
     }
 }
-
-// console.log(operate("-", 9, 5));
