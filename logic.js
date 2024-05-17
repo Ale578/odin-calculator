30eff3c6dc2ec3c4f7e6a693c669b23d3996cc7f
Ale579 <alessandrotg9@gmail.com>
2024-05-17 16:00:34 +0200

    Simplify operate function

--- logic.js
+++ logic.js
@@ -116,31 +116,15 @@ sign.addEventListener("click", () => {
     }
 });
 
-function add (a, b) {
-    return a + b;
-}
-
-function subtract (a, b) {
-    return a - b;
-}
-
-function multiply (a, b) {
-    return a * b;
-}
-
-function divide (a, b) {
-    return a / b;
-}
-
 function operate (operator, a, b) {
     switch (operator) {
         case "+":
-            return add(a, b);
+            return a * b;
         case "-":
-            return subtract(a, b);
+            return a - b;
         case "*":
-            return multiply(a, b);
+            return a * b;
         case "/":
-            return divide(a, b);
+            return a / b;
     }
 }
